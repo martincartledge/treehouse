8b29acc447f8be2f671be147830a61ab7da2c9a9
Martin Cartledge <martincartledge@icloud.com>
2017-08-02 09:58:44 -0700

    add express files

---
+++ express/Practice-Express-master/README.md
@@ -0,0 +1,26 @@
+# Practicing Express
+
+
+*nodemon*
+
+`npm install -g nodemon`
+
+*node-inspector*
+
+`npm install -g node-inspector`
+
+##### Run debugger and nodemon simultaneously
+
+`node-inspector`
+
+*new tab*
+
+`nodemon --debug src/app.js`
+
+*break on first line*
+
+`nodemon --debug-brk src/app.js`
+
+*install jade*
+
+`npm install jade --save`
---
+++ express/Practice-Express-master/src/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express'),
+    posts = require('./mock/posts.json');
+
+var postsLists = Object.keys(posts).map(function(value){
+  return posts[value];
+});
+
+var app = express();
+
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/blog/:title?', function(req, res){
+  var title = req.params.title;
+  if (title === undefined) {
+    res.status(503);
+    res.render('blog', { posts: postsLists });
+  } else {
+    var post = posts[title] || {};
+    res.render('post', { post: post });
+  }
+});
+
+app.listen(5000, function(){
+  console.log("The frontend server is running on port 5000");
+});
