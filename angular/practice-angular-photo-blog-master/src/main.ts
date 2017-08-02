91b36428203b87680296a2ebb1723ab530d485b5
Martin Cartledge <martincartledge@icloud.com>
2017-08-02 09:50:37 -0700

    remove angular from master

--- angular/practice-angular-photo-blog-master/src/main.ts
+++
@@ -1,6 +0,0 @@
-// Import global stylesheet
-import './styles/main.css';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
